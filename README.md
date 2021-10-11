1320ca441c1c75a56c964792cce485515ed72e81
Jesús Leal <leal.perez@bootcamp-upgrade.com>
2021-10-11 02:26:42 +0200

    FEAT: Some minor issues solved

---
+++ README.md
@@ -0,0 +1,22 @@
+### Bootcamp Full Stack Developer - Upgrade Hub
+## Proyecto JavaScript - MoleBuster
+---
+
+Página Web creada utilizando HTML5, SCSS (CSS) y JavaScript como proyecto del segundo módulo del bootcamp.
+
+La idea es crear una web con uno de los juegos más famosos de la historia: **MoleBuster**. Para ello se genera una matriz (array bidimensional) que empieza con la imagen de un agujero y cada 'x' segundos cambia esa imagen por la de un Castor. Si el usuario hace click sobre la imagen, suma uno en su contador.
+
+* Todo el código fuente está disponible en el repositorio:
+[https://gitlab.com/jleal22/proyecto-javascript-molebuster-jesus-leal](https://gitlab.com/jleal22/proyecto-javascript-molebuster-jesus-leal)
+
+![View web](./images/captura2.jpg)
+...
+
+**Estructura:**
+
+Componentes creados:
+- index.html
+- index.js
+- styles/styles.scss
+- styles/style.css (Generado a partir de styles.scss)
+- images/*
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let isPlaying = false;
 let point1 = 0;
 let point2 = 0;
-let chosenMole = '';
+let chosenMole = "";
 let timeoutID = 0;
 let attemptsCounter = 0;
 const maxNumberMole = 20;
@@ -13,209 +13,200 @@ const nPause = 900;
 // Initialize game
 //
 function init() {
-
-    hideGameOver();
-    includeHoles(maxNumberMole);
-    prepareButtons();
-
+  hideGameOver();
+  includeHoles(maxNumberMole);
+  prepareButtons();
 }
 
 //
 // Initial screen - Include holes on game dashboard
 //
 function includeHoles(times) {
+  console.log("includeHoles");
+  const $$game = window.document.querySelector("#game");
 
-    console.log('includeHoles');
-    const $$game = window.document.querySelector('#game');
+  for (let i = 1; i <= times; i++) {
+    const $$cell = document.createElement("div");
+    $$cell.className = "game__cell";
 
-    for (let i = 1; i <= times; i++) {
+    const $$img = document.createElement("img");
+    $$img.src = "/images/hole-svgrepo-com.svg";
+    $$img.alt = "Mole";
+    $$img.className = "game__img";
+    $$img.id = "game__img_" + i;
+    $$img.addEventListener("click", clickImg);
 
-        const $$cell = document.createElement('div');
-        $$cell.className = "game__cell";
-        
-        const $$img = document.createElement('img');
-        $$img.src = "/images/hole-svgrepo-com.svg";
-        $$img.alt = "Mole";
-        $$img.className="game__img";
-        $$img.id = "game__img_" + i;
-        $$img.addEventListener('click', clickImg);
-
-        $$cell.appendChild($$img);
-        $$game.appendChild($$cell);
-    }
+    $$cell.appendChild($$img);
+    $$game.appendChild($$cell);
+  }
 }
 
 //
 // Prepare play and cancel buttons
 //
 function prepareButtons() {
-
-    console.log('prepareButtons');
-    window.document.querySelector('#game__btn-init').addEventListener('click', clickPlayButton);
-    window.document.querySelector('#game__btn-canc').addEventListener('click', clickCancelButton);
+  console.log("prepareButtons");
+  window.document
+    .querySelector("#game__btn-init")
+    .addEventListener("click", clickPlayButton);
+  window.document
+    .querySelector("#game__btn-canc")
+    .addEventListener("click", clickCancelButton);
 }
 
 //
 // Play game
 //
 function playGame(nTimes) {
+  console.log("playGame");
 
-    console.log('playGame');
-    
-    chosenMole = 'game__img_' + selectRamdomMole(maxNumberMole);
-    window.document.getElementById(chosenMole).src = "/images/mole-svgrepo-com.svg";
-    timeoutID = setTimeout(showKo, nTimeout, chosenMole);
+  chosenMole = "game__img_" + selectRamdomMole(maxNumberMole);
+  window.document.getElementById(chosenMole).src =
+    "/images/mole-svgrepo-com.svg";
+  timeoutID = setTimeout(showKo, nTimeout, chosenMole);
 }
 
 //
 // Function to be executed when clicking on a mole
 //
 function clickImg(event) {
+  console.log("clickImg");
+  clearTimeout(timeoutID);
 
-    console.log('clickImg');
-    clearTimeout(timeoutID);
-
-    if ( event.target.id == chosenMole) {
-        console.log("BIEEEN! Es el mismo");
-        showOk(event.target.id);
-    } else {
-        console.log("OHHHH! No es el correcto");
-        showKo(event.target.id);
-    }
-
+  if (event.target.id == chosenMole) {
+    console.log("BIEEEN! Es el mismo");
+    showOk(event.target.id);
+  } else {
+    console.log("OHHHH! No es el correcto");
+    showKo(event.target.id);
+  }
 }
 
 //
 // Function to be executed when clicking on the start button
 //
 function clickPlayButton() {
-    console.log('clickPlayButton');
+  console.log("clickPlayButton");
 
-    if (!isPlaying) {
-        isPlaying = true;
-        resetPoints();
-        cleanGamePanel();
-        hideGameOver();
-        playGame();
-    } else {
-        console.error('Ya está jugando');
-    }
+  if (!isPlaying) {
+    isPlaying = true;
+    attemptsCounter = 0;
+    resetPoints();
+    cleanGamePanel();
+    hideGameOver();
+    playGame();
+  } else {
+    console.error("Ya está jugando");
+  }
 }
 
 //
 // Function to be executed when clicking on the cancel button
 //
 function clickCancelButton() {
-
-    console.log('clickCancelButton');
+  console.log("clickCancelButton");
+  if (isPlaying) {
     isPlaying = false;
     clearTimeout(timeoutID);
     cleanGamePanel();
     showGameOver();
+  }
 }
 
 //
 // Player has clicked on a hole and not on the mole
 //
 function showKo(elem) {
-    
-    console.log('showKo ->', elem);
-    $$elem = window.document.querySelector('#' + elem);
-    $$elem.src = "/images/error-svgrepo-com.svg";
-    addPointKo();
-    playAgain();
+  console.log("showKo ->", elem);
+  $$elem = window.document.querySelector("#" + elem);
+  $$elem.src = "/images/error-svgrepo-com.svg";
+  addPointKo();
+  playAgain();
 }
 
 //
 // Player has clicked on the mole
 //
 function showOk(elem) {
-
-    console.log('showOk');
-    $$elem = window.document.querySelector('#' + elem);
-    $$elem.src = "/images/success-svgrepo-com.svg";
-    addPointOk();
-    playAgain();
+  console.log("showOk");
+  $$elem = window.document.querySelector("#" + elem);
+  $$elem.src = "/images/success-svgrepo-com.svg";
+  addPointOk();
+  playAgain();
 }
 
 //
 // Play successive games
 //
 function playAgain() {
+  attemptsCounter++;
 
-    attemptsCounter++;
-
-    if (attemptsCounter < attempts) {
-
-        timeoutID = setTimeout(() => {
-            cleanGamePanel();
-            playGame();
-        }, 1000);
-
-    } else {
-        isPlaying = false;
-        attemptsCounter = 0;
-        showGameOver()
-    }     
+  if (attemptsCounter < attempts) {
+    timeoutID = setTimeout(() => {
+      cleanGamePanel();
+      playGame();
+    }, 1000);
+  } else {
+    isPlaying = false;
+    attemptsCounter = 0;
+    showGameOver();
+  }
 }
 
 function addPointOk() {
-    console.log('addPointOk');
-    point1++;
-    updatePointOk(point1);
+  console.log("addPointOk");
+  point1++;
+  updatePointOk(point1);
 }
 
 function addPointKo() {
-    console.log('addPointKo');
-    point2++;
-    updatePointKo(point2);
+  console.log("addPointKo");
+  point2++;
+  updatePointKo(point2);
 }
 
 function resetPoints() {
-    console.log('resetPointOk');
-    point1=0;
-    point2=0;
-    updatePointOk(point1);
-    updatePointKo(point2);
+  console.log("resetPointOk");
+  point1 = 0;
+  point2 = 0;
+  updatePointOk(point1);
+  updatePointKo(point2);
 }
 
 function updatePointOk(value) {
-    window.document.querySelector('#game__points-1').textContent = value;
+  window.document.querySelector("#game__points-1").textContent = value;
 }
 
 function updatePointKo(value) {
-    window.document.querySelector('#game__points-2').textContent = value;
+  window.document.querySelector("#game__points-2").textContent = value;
 }
- 
+
 //
 // Randomly selects the mole to be played with
 //
 function selectRamdomMole(maxValue) {
-
-    return (Math.round((maxValue - 1) * Math.random()) + 1);
+  return Math.round((maxValue - 1) * Math.random()) + 1;
 }
 
 function cleanGamePanel() {
-
-    console.log("cleanGamePanel");
-    const $$cells = document.querySelectorAll('.game__img');
-    $$cells.forEach((elem) => {
-        elem.src = "/images/hole-svgrepo-com.svg";
-    });
+  console.log("cleanGamePanel");
+  const $$cells = document.querySelectorAll(".game__img");
+  $$cells.forEach((elem) => {
+    elem.src = "/images/hole-svgrepo-com.svg";
+  });
 }
 
 function showGameOver() {
-    console.log('showGameOver');
-    document.querySelector('.game__over').classList.remove('d-none');
+  console.log("showGameOver");
+  document.querySelector(".game__over").classList.remove("d-none");
 }
 
 function hideGameOver() {
-    console.log('hideGameOver');
-    document.querySelector('.game__over').classList.add('d-none');
+  console.log("hideGameOver");
+  document.querySelector(".game__over").classList.add("d-none");
 }
 
 window.onload = () => {
-    
-    console.log("Onload");
-    init();
-}
+  console.log("Onload");
+  init();
+};
